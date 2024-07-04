03f2a34448dd9be8b8a11e08c8664b040f8d563b
Amanda Flavia <flaviacicada@gmail.com>
2024-07-04 11:39:01 +0300

    add: adds Potfolio.h data and methods

---
+++ portfolio_operations.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+struct Portfolio {
+    int user_id;
+    int company_id;
+    int shares_owned;
+};
+
+bool insertPortfolioEntry(const Portfolio& portfolio);
+bool updatePortfolioEntry(const Portfolio& portfolio);
+bool deletePortfolioEntry(int user_id, int company_id);
+std::vector<Portfolio> getPortfoilioForUser(int user_id); 
