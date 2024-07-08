fc6aa358c38b7c947b6dce5cab7ff84e399c47ad
Amanda Flavia <flaviacicada@gmail.com>
2024-07-08 10:47:04 +0300

    add: adds Function to update an existing portfolio entry

--- portfolio_operations.cpp
+++ portfolio_operations.cpp
@@ -25,64 +25,69 @@ bool connectToDatabase(SQLHANDLE &henv, SQLHANDLE &hdbc)
         return false;
     }
 
-     // Allocate connection handle
-     ret = SQLAllocHandle(SQL_HANDLE_DBC,henv, &hdbc);
-    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO){
-        std::cerr <<"Error allocating connecting handle" <<std::endl;
+    // Allocate connection handle
+    ret = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc);
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
+        std::cerr << "Error allocating connecting handle" << std::endl;
         SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
         SQLFreeHandle(SQL_HANDLE_ENV, henv);
         return false;
     }
 
-    SQLWCHAR* connStr = (SQLWCHAR*)"DRIVER={ODBC Driver 18 for SQL Server};SERVER=moneytrees-sqlserver.database.windows.net;DATABASE=MoneyTreesDatabase;UID=Flavia;PWD=Darken&5h33p !;";
+    SQLWCHAR *connStr = (SQLWCHAR *)"DRIVER={ODBC Driver 18 for SQL Server};SERVER=moneytrees-sqlserver.database.windows.net;DATABASE=MoneyTreesDatabase;UID=Flavia;PWD=Darken&5h33p !;";
 
-    //Connect to the database
-    ret = SQLConnect(hdbc, connStr, SQL_NTS, NULL, 0, NULL,0 );
-    if(ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO){
-        std::cerr <<"Error connecting to the database" <<std::endl;
+    // Connect to the database
+    ret = SQLConnect(hdbc, connStr, SQL_NTS, NULL, 0, NULL, 0);
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
+        std::cerr << "Error connecting to the database" << std::endl;
         SQLFreeHandle(SQL_HANDLE_ENV, henv);
         return false;
     }
     return true;
 }
 
-bool insertPortfoilioEntry(const Portfolio& portfolio){
+bool insertPortfoilioEntry(const Portfolio &portfolio)
+{
     SQLHANDLE henv, hdbc, hstmt;
     SQLRETURN ret;
 
-    if (!connectToDatabase(henv, hdbc)){
+    if (!connectToDatabase(henv, hdbc))
+    {
         return false;
     }
     ret = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
-    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO){
-        std::cerr << "Error alloccating Statement Handle" <<std::endl;
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
+        std::cerr << "Error alloccating Statement Handle" << std::endl;
         SQLDisconnect(hdbc);
         SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
-         SQLFreeHandle(SQL_HANDLE_ENV, henv);
-         return false;
-    } 
+        SQLFreeHandle(SQL_HANDLE_ENV, henv);
+        return false;
+    }
 
     std::string sqlQuery = "INSERT INTO portfoilios (user_id, company_id, shares_owned) VALUES(?, ?, ?)";
-    ret = SQLPrepare(hstmt, (SQLWCHAR*) sqlQuery.c_str(), SQL_NTS);
-    if(ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO){
-        std::cerr << "Error preparing SQL statement" <<std::endl;
+    ret = SQLPrepare(hstmt, (SQLWCHAR *)sqlQuery.c_str(), SQL_NTS);
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
+        std::cerr << "Error preparing SQL statement" << std::endl;
         SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
         SQLDisconnect(hdbc);
         SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
         SQLFreeHandle(SQL_HANDLE_ENV, henv);
         return false;
-
     }
 
-    ret = SQLBindParameter(hstmt, 1, SQL_PARAM_INPUT, SQL_C_LONG, SQL_INTEGER, 0, 0, (SQLPOINTER)& portfolio.user_id,0 ,NULL);
-    ret = SQLBindParameter(hstmt, 2, SQL_PARAM_INPUT, SQL_C_LONG, SQL_INTEGER,0,0 ,(SQLPOINTER) &portfolio.company_id, 0, NULL);
-    ret = SQLBindParameter(hstmt, 3, SQL_PARAM_INPUT, SQL_C_LONG, SQL_INTEGER, 0, 0 ,(SQLPOINTER)& portfolio.shares_owned, 0 ,NULL);
-
+    ret = SQLBindParameter(hstmt, 1, SQL_PARAM_INPUT, SQL_C_LONG, SQL_INTEGER, 0, 0, (SQLPOINTER)&portfolio.user_id, 0, NULL);
+    ret = SQLBindParameter(hstmt, 2, SQL_PARAM_INPUT, SQL_C_LONG, SQL_INTEGER, 0, 0, (SQLPOINTER)&portfolio.company_id, 0, NULL);
+    ret = SQLBindParameter(hstmt, 3, SQL_PARAM_INPUT, SQL_C_LONG, SQL_INTEGER, 0, 0, (SQLPOINTER)&portfolio.shares_owned, 0, NULL);
 
     ret = SQLExecute(hstmt);
 
-    if(ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO){
-        std::cerr <<"Error executing SQL statement" <<std::endl;
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
+        std::cerr << "Error executing SQL statement" << std::endl;
         SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
         SQLDisconnect(hdbc);
         SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
@@ -94,10 +99,62 @@ bool insertPortfoilioEntry(const Portfolio& portfolio){
     SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
     SQLFreeHandle(SQL_HANDLE_ENV, henv);
     return true;
-
-
 }
 
-bool updatePortfolioEntry(const Portfolio &portfolio){
+bool updatePortfolioEntry(const Portfolio &portfolio)
+{
+    SQLHANDLE henv, hdbc, hstmt;
+    SQLRETURN ret;
 
+    if (!connectToDatabase(henv, hdbc))
+    {
+        return false;
+    }
+
+    ret = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
+
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
+        std::cerr << "Error Allocating Statement Handle" << std::endl;
+        SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+        SQLDisconnect(hdbc);
+        SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+        SQLFreeHandle(SQL_HANDLE_ENV, henv);
+        return false;
+    }
+
+    std::string sqlQuery = "UPDATE portfolios SET shares_owned= ? WHERE user_id = ? AND company_id = ?";
+    ret = SQLPrepare(hstmt, (SQLWCHAR *)sqlQuery.c_str(), SQL_NTS);
+
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
+        std::cerr << "Error Preparing SQL Statement" << std::endl;
+        SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+        SQLDisconnect(hdbc);
+        SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+        SQLFreeHandle(SQL_HANDLE_ENV, henv);
+        return false;
+    }
+
+    ret = SQLBindParameter(hstmt, 1, SQL_PARAM_INPUT, SQL_C_LONG, SQL_INTEGER, 0, 0, (SQLPOINTER)&portfolio.user_id, 0, NULL);
+    ret = SQLBindParameter(hstmt, 2, SQL_PARAM_INPUT, SQL_C_LONG, SQL_INTEGER, 0, 0, (SQLPOINTER)&portfolio.company_id, 0, NULL);
+    ret = SQLBindParameter(hstmt, 3, SQL_PARAM_INPUT, SQL_C_LONG, SQL_INTEGER, 0, 0, (SQLPOINTER)&portfolio.shares_owned, 0, NULL);
+
+    ret = SQLExecute(hstmt);
+
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
+        std::cerr << "Error executing SQL statement" << std::endl;
+        SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+        SQLDisconnect(hdbc);
+        SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+        SQLFreeHandle(SQL_HANDLE_ENV, henv);
+        return false;
+    }
+
+    SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+    SQLDisconnect(hdbc);
+    SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+    SQLFreeHandle(SQL_HANDLE_ENV, henv);
+    return true;
 }
