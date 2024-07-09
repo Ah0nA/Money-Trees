bd5a08405ed39266047fa1b0a05621ef803e3b5f
Amanda Flavia <flaviacicada@gmail.com>
2024-07-09 15:49:19 +0300

    add:adds files

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+#include <cpprest/json.h>
+#include "portfolio_operations.h"
+#include "esg_impact_calculation.h"
+#include "ranking_system.h"
+#include <iostream>
+
+
